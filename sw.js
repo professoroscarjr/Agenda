d92ab62c102ce599b68fd62b1aa843a1c67a0e91
professoroscarjr <139995876+professoroscarjr@users.noreply.github.com>
2025-08-06 10:18:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-task-list-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/Agenda/',
+    '/Agenda/index.html',
+    '/Agenda/style.css',
+    '/Agenda/manifest.json',
+    '/Agenda/icons/icon-192x192.png',
+    '/Agenda/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -24,4 +24,5 @@ self.addEventListener('fetch', event => {
                 return response || fetch(event.request);
             })
     );
+
 });
